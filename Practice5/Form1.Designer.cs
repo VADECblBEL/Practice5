82f6909d82766da04a6acecb232c9f35aba4da8c
student <student@C383U20>
2021-03-09 17:46:19 +0300

    Добавьте файлы проекта.

---
+++ Practice5/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Practice5
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Обязательная переменная конструктора.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Освободить все используемые ресурсы.
+    /// </summary>
+    /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Код, автоматически созданный конструктором форм Windows
+
+    /// <summary>
+    /// Требуемый метод для поддержки конструктора — не изменяйте 
+    /// содержимое этого метода с помощью редактора кода.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.listBox1 = new System.Windows.Forms.ListBox();
+      this.button1 = new System.Windows.Forms.Button();
+      this.label1 = new System.Windows.Forms.Label();
+      this.SuspendLayout();
+      // 
+      // listBox1
+      // 
+      this.listBox1.FormattingEnabled = true;
+      this.listBox1.Items.AddRange(new object[] {
+            "Добрый день",
+            "Информационные технологии",
+            "Раз два три",
+            "Тридцать три коровы",
+            "Изучаем редактор коллекции строк"});
+      this.listBox1.Location = new System.Drawing.Point(70, 40);
+      this.listBox1.Name = "listBox1";
+      this.listBox1.Size = new System.Drawing.Size(424, 199);
+      this.listBox1.TabIndex = 0;
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(70, 300);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(152, 63);
+      this.button1.TabIndex = 1;
+      this.button1.Text = "Пуск";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // label1
+      // 
+      this.label1.Location = new System.Drawing.Point(482, 306);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(172, 51);
+      this.label1.TabIndex = 2;
+      this.label1.Text = "Необходимо найти долю пробелов в строке";
+      this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(800, 450);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.button1);
+      this.Controls.Add(this.listBox1);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      this.ResumeLayout(false);
+
+    }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Practice5/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practice5
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+      label1.Text = "Необходимо найти долю пробелов в строке";
+        }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      int index = listBox1.SelectedIndex;
+      string str = (string)listBox1.Items[index];
+      int len = str.Length;
+      int count = 0;
+      
+      int i = 0;
+      while (i < len - 1)
+      {
+          if (str[i] == ' ')
+          count++;
+        i++;
+      }
+
+      double dolya = count / len;
+      label1.Text = "Доля пробелов в строке: " + count.ToString() + "/" + len.ToString();
+
+    }
+  }
+}
